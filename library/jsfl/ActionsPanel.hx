3bff3c1f2b0816ac0dac29d4adf02d416812c407
Yaroslav Sivakov <yar3333@gmail.com>
2014-12-23 19:47:23 +0300

    * old library sources: remove "public"++

--- library/jsfl/ActionsPanel.hx
+++ library/jsfl/ActionsPanel.hx
@@ -2,13 +2,13 @@ package jsfl;
 
 extern class ActionsPanel
 {
-	public function getClassForObject(ASvariableName:String) : String;
-	public function getScriptAssistMode() : Bool;
-	public function getSelectedText() : String;
-	public function getText() : String;
-	public function hasSelection() : Bool;
-	public function replaceSelectedText(replacementText:String) : Bool;
-	public function setScriptAssistMode(bScriptAssist:Bool) : Void;
-	public function setSelection(startIndex:Int, numberOfChars:Int) : Bool;
-	public function setText(replacementText:String) : Bool;
+	function getClassForObject(ASvariableName:String) : String;
+	function getScriptAssistMode() : Bool;
+	function getSelectedText() : String;
+	function getText() : String;
+	function hasSelection() : Bool;
+	function replaceSelectedText(replacementText:String) : Bool;
+	function setScriptAssistMode(bScriptAssist:Bool) : Void;
+	function setSelection(startIndex:Int, numberOfChars:Int) : Bool;
+	function setText(replacementText:String) : Bool;
 }
