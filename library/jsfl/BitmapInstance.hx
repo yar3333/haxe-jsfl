f26885dbe52af876a6df7f2e1e86c558629451db
Yaroslav Sivakov <yar3333@gmail.com>
2014-12-23 19:52:20 +0300

    * extract BitmapInstanceBits to separate file

--- library/jsfl/BitmapInstance.hx
+++ library/jsfl/BitmapInstance.hx
@@ -1,18 +1,9 @@
 package jsfl;
 
-typedef BitmapInstanceBits =
-{
-	var width : Int;
-	var height : Int;
-	var depth : Int;
-	var bits : Array<Int>;
-	var cTab : Array<String>;
-}
-
 typedef BitmapInstance =
 {> Instance,
-	function getBits() : BitmapInstanceBits;
 	var hPixels : Int;
-	function setBits(bitmap:BitmapInstanceBits) : Void;
 	var vPixels : Int;
+	function getBits() : BitmapInstanceBits;
+	function setBits(bitmap:BitmapInstanceBits) : Void;
 }
